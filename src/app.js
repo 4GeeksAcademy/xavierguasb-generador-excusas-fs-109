9109d71d705cbfbc62e4f69145b0b7f56ab43cd1
Xavier Guas <xavier.guas@gmail.com>
2025-05-09 18:27:31 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = excusas ();
+  };
+
+function excusas () {
+
+  let who = ['The dog ', 'My grandma ', 'The mailman ', 'My bird '];
+  let action = ['ate ', 'peed ', 'crushed ', 'broke '];
+  let what = ['my homework ', 'my phone ', 'the car '];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let whoR = who[Math.floor(Math.random() * who.length)];
+  let actionR = action[Math.floor(Math.random() * action.length)];
+  let whatR = what[Math.floor(Math.random() * what.length)];
+  let whenR = when[Math.floor(Math.random() * when.length)];
+
+return whoR + "" + actionR + "" + whatR + "" + whenR;
+
 };
+
+  console.log("Hello Rigo from the console!");
+
