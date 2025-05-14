4921822001ab5f7e4553c21861e82e4a2b9c0237
Xavier Guas <xavier.guas@gmail.com>
2025-05-14 15:09:03 +0000

    modificado con funcion para reducir los Math

--- src/app.js
+++ src/app.js
@@ -6,24 +6,53 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  document.getElementById("excuse").innerHTML = excusas ();
+
+let who = ['The dog ', 'My grandma ', 'The mailman ', 'My bird '];
+let action = ['ate ', 'peed ', 'crushed ', 'broke '];
+let what = ['my homework ', 'my phone ', 'the car '];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+let whoR = obtenerNumeroAleatorio(who);
+let actionR = obtenerNumeroAleatorio(action);
+let whatR = obtenerNumeroAleatorio(what);
+let whenR = obtenerNumeroAleatorio(when);
+
+let randomsentence = whoR + "" + actionR + "" + whatR + "" + whenR;
+
+  // document.getElementById("excuse").innerHTML = randomsentence ();
+
+let resultado = document.getElementById("excuse");
+resultado.innerHTML = randomsentence;
   };
 
-function excusas () {
+function obtenerNumeroAleatorio (aleatorio){
 
-  let who = ['The dog ', 'My grandma ', 'The mailman ', 'My bird '];
-  let action = ['ate ', 'peed ', 'crushed ', 'broke '];
-  let what = ['my homework ', 'my phone ', 'the car '];
-  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
-
-  let whoR = who[Math.floor(Math.random() * who.length)];
-  let actionR = action[Math.floor(Math.random() * action.length)];
-  let whatR = what[Math.floor(Math.random() * what.length)];
-  let whenR = when[Math.floor(Math.random() * when.length)];
-
-return whoR + "" + actionR + "" + whatR + "" + whenR;
+  return aleatorio[Math.floor(Math.random() * aleatorio.length)];
 
 };
 
+
+// function excusas () {
+
+  // let who = ['The dog ', 'My grandma ', 'The mailman ', 'My bird '];
+  // let action = ['ate ', 'peed ', 'crushed ', 'broke '];
+  // let what = ['my homework ', 'my phone ', 'the car '];
+  // let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+//   let whoR = who[Math.floor(Math.random() * who.length)];
+//   let actionR = action[Math.floor(Math.random() * action.length)];
+//   let whatR = what[Math.floor(Math.random() * what.length)];
+//   let whenR = when[Math.floor(Math.random() * when.length)];
+
+// return whoR + "" + actionR + "" + whatR + "" + whenR;
+
+// };
+
+
+
+
+
   console.log("Hello Rigo from the console!");
+
+
 
